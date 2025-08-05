ce6ee3108660365f75624de985bd9a51047190b4
sidharaj manek <sidharajmanek308@gmail.com>
2025-08-05 22:47:49 +0530

    segmentation.py

--- segmentation.py
+++ segmentation.py
@@ -1,7 +1,17 @@
 from ultralytics import YOLO
 import cv2
+import os
 
-model = YOLO("yolov8m-seg.pt")
+# Path to store the model
+model_path = "yolov8m-seg.pt"
+
+# Download model if not present
+if not os.path.exists(model_path):
+    from ultralytics.hub import download
+    download("yolov8m-seg.pt", model_path)
+
+# Load YOLOv8 model
+model = YOLO(model_path)
 
 def segment_image(image):
     results = model(image, verbose=False)
