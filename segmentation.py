a9ed5601da33bdaae04c3484896476f3b772fa9e
sidharaj manek <sidharajmanek308@gmail.com>
2025-08-05 22:40:27 +0530

    segmentation.py

--- segmentation.py
+++ segmentation.py
@@ -1,11 +1,8 @@
 from ultralytics import YOLO
-   import cv2
+import cv2
 
-   # Load YOLOv8 model (pre-trained on COCO)
-   model = YOLO("yolov8m-seg.pt")
+model = YOLO("yolov8m-seg.pt")
 
-   def segment_image(image):
-       """Segment objects in the image using YOLOv8."""
-       results = model(image, verbose=False)
-       return results[0].masks.data.cpu().numpy() if results[0].masks is not None 
-       else None
+def segment_image(image):
+    results = model(image, verbose=False)
+    return results[0].masks.data.cpu().numpy() if results[0].masks is not None else None
