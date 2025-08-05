5e133fa835223e1650b1c4997bc592aef93ffad1
sidharaj manek <sidharajmanek308@gmail.com>
2025-08-05 22:41:15 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import cv2
-   import numpy as np
+import numpy as np
 
-   def load_image(image_path):
-       return cv2.imread(image_path)
+def load_image(image_path):
+    return cv2.imread(image_path)
 
-   def display_results(image, masks, caption):
-       return image, caption
+def display_results(image, masks, caption):
+    return image, caption
