b669d43288cd9d3b0b854edf9665c979d995f5c1
sidharaj manek <sidharajmanek308@gmail.com>
2025-09-02 23:06:24 +0530

    captioning.py

--- captioning.py
+++ captioning.py
@@ -1,18 +1,14 @@
-import tensorflow as tf
-from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
-import numpy as np
+from transformers import BlipProcessor, BlipForConditionalGeneration
+import torch
+from PIL import Image
 
-model = InceptionV3(weights='imagenet')
-model_new = tf.keras.Model(model.input, model.layers[-2].output)
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
-def extract_features(image_path):
-    image = load_img(image_path, target_size=(299, 299))
-    image = img_to_array(image)
-    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-    image = preprocess_input(image)
-    feature = model_new.predict(image, verbose=0)
-    return feature
-
-def generate_caption(features):
-    return "A couple of cats lying on a couch"
+def generate_caption(image_path):
+    image = Image.open(image_path)
+    inputs = processor(image, return_tensors="pt")
+    with torch.no_grad():
+        output = model.generate(**inputs)
+    caption = processor.decode(output[0], skip_special_tokens=True)
+    return caption
